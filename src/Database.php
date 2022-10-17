c173f0ca307d5d77d474f710a02e0c0e6832441e
Hadaly Villasclaras <hadalyvf@gmail.com>
2022-10-17 14:12:15 +0200

    Update Database.php

--- src/Database.php
+++ src/Database.php
@@ -30,8 +30,8 @@ class Database
 
             $this->conn = new PDO($dsn, $this->user, $this->password, [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_EMULATE_PREPARES => false, //deshabilita la emulacion de sentencias preparadas
-                PDO::ATTR_STRINGIFY_FETCHES => false //convierte todos los valores a strings x defecto (si true)
+                PDO::ATTR_EMULATE_PREPARES => false,
+                PDO::ATTR_STRINGIFY_FETCHES => false
             ]);
         }
 
