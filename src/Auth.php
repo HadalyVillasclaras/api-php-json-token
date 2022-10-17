b1c92268747dd62c0b639df203ea7fb7b0a1dcc0
Hadaly Villasclaras <hadalyvf@gmail.com>
2022-10-17 14:11:40 +0200

    Update Auth.php

--- src/Auth.php
+++ src/Auth.php
@@ -1,5 +1,11 @@
 <?php
 
+/*
+ * Authentication methods:
+ * - by API key
+ * - by Access token
+*/
+
 class Auth
 {
     private $userGateway;
@@ -8,13 +14,11 @@ class Auth
     public function __construct(/*private*/ UserGateway $userGateway)
     {
         $this->userGateway = $userGateway;
-        
     }
-
+    
     public function authenticateAPIKey(): bool
     {
         if (empty($_SERVER["HTTP_X_API_KEY"])) {
-
             http_response_code(400);
             echo json_encode(["message" => "missing API key"]);
             return false;
@@ -24,8 +28,6 @@ class Auth
 
         $user = $this->userGateway->getByAPIKey($apiKey);
 
-
-        //check authentication of api key
         if ($user === false) {
             http_response_code(401); //401 unauthorized
             echo json_encode(["message" => "invalid API key"]);
@@ -37,31 +39,26 @@ class Auth
         return true;
     }
 
-    public function getUserId(): int
-    {
-        return $this->userId;
-    }
-
     public function authenticateAccessToken(): bool
     {
+        //check if if authentication matches the scheme
         if (!preg_match("/^Bearer\s+(.*)$/", $_SERVER["HTTP_AUTHORIZATION"], $matches)) {
             http_response_code(400);
             echo json_encode(["message" => "incomplete authorization header"]);
             return false;
         }
 
-        $plainText = base64_decode($matches[1], true);
+        $decodeMatch = base64_decode($matches[1], true);
 
-        if ($plainText === false) {
+        if ($decodeMatch === false) {
             http_response_code(400);
             echo json_encode(["message" => "invalid authorization header"]);
             return false;
         }
 
-        $data = json_decode($plainText, true);
+        $data = json_decode($decodeMatch, true);
 
-        if ($data === null)
-        {
+        if ($data === null) {
             http_response_code(400);
             echo json_encode(["message" => "invalid JSON"]);
             return false;
@@ -69,7 +66,11 @@ class Auth
 
         $this->userId = $data["id"];
 
-        print_r($data);
         return true;
+    }
+
+    public function getUserId(): int
+    {
+        return $this->userId;
     }
 }
