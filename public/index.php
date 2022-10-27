87d8a9bf83e60e7897e60743010c080c00dea7a1
Hadaly Villasclaras <hadalyvf@gmail.com>
2022-10-27 08:22:07 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -2,13 +2,8 @@
 
 declare(strict_types=1);
 
-use App\Authentication;
-use App\JWTCodec;
 use App\Task\Controller\TaskController;
 use App\User\Controller\UserController;
-
-use App\Task\Infrastructure\TaskRepository;
-use App\UserGateway;
 
 require dirname(__DIR__) . "/src/bootstrap.php";
 
@@ -19,11 +14,10 @@ $dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
     $r->addRoute('POST', '/my_api/public/tasks', 'TaskController::addByUserId');
     $r->addRoute('PATCH', '/my_api/public/tasks/{id}', 'TaskController::updateByUserId');
     $r->addRoute('DELETE', '/my_api/public/tasks/{id}', 'TaskController::deleteByUserId');
-
+    
+    $r->addRoute('POST', '/my_api/public/user/logout', 'UserController::logout');
     $r->addRoute('POST', '/my_api/public/user/register', 'UserController::register');
     $r->addRoute('POST', '/my_api/public/user/login', 'UserController::login');
-
-
 });
 
 $httpMethod = $_SERVER['REQUEST_METHOD'];
@@ -33,27 +27,30 @@ if (false !== $pos = strpos($uri, '?')) {
     $uri = substr($uri, 0, $pos);
 }
 
-$uri = rawurldecode($uri);  // /my_api/public/tasks
-
+$uri = rawurldecode($uri);
 $routeInfo = $dispatcher->dispatch($httpMethod, $uri);
 
+$controllerMethod = explode("::",  $routeInfo[1]);
+$controller = $controllerMethod[0];
+$method = $controllerMethod[1];
+$vars = $routeInfo[2];
+
+// Process request
 switch ($routeInfo[0]) {
+
     case \FastRoute\Dispatcher::NOT_FOUND:
         // ... 404 Not Found
         echo 'not found';
         break;
+
     case \FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
         // ... 405 Method Not Allowed
         echo 'not found';
         break;
-    case \FastRoute\Dispatcher::FOUND:
-        $controllerMethod = explode("::",  $routeInfo[1]);
-        $controller = $controllerMethod[0];
-        $method = $controllerMethod[1];
-        $vars = $routeInfo[2];
 
+    case \FastRoute\Dispatcher::FOUND:
         if ($controller == "TaskController") {
-            $controllerToLoad = new TaskController($userId);
+            $controllerToLoad = new TaskController();
         } elseif ($controller == "UserController") {
             $controllerToLoad = new UserController();
         }
@@ -63,29 +60,5 @@ switch ($routeInfo[0]) {
         } else {
             $controllerToLoad->$method(); 
         }
-
         break;
 }
-
-exit;
-
-
-// User authentication 
-$userGateway = new UserGateway();
-$codec = new JWTCodec();
-$auth = new Authentication($userGateway, $codec);
-
-// if (!$auth->authenticateAccessToken()) {
-//     exit;
-// }
-
-// if (!$auth->authenticateAPIKey()) {
-//     exit;
-// }
-
-if (!$auth->authenticateJwtToken()) {
-    exit;
-}
-
-// Process request
-$userId = $auth->getUserId();
